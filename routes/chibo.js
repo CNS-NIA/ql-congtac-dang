d6a463db0a37223c5ec55412f6a0b0cf1bde0352
CNS-NIA <naoc.cns@gmail.com>
2026-02-05 19:41:49 +0700

    Create chibo.js

---
+++ routes/chibo.js
@@ -0,0 +1,13 @@
+{
+  "lastId": 1,
+  "chibos": [
+    {
+      "id": 1,
+      "maChiBo": "CB_TB_TTDD",
+      "tenChiBo": "Chi bộ Đội Thiết bị Thông tin Dẫn đường",
+      "dangBoId": 1,
+      "soDangVien": 15,
+      "createdAt": "2024-01-01T00:00:00Z"
+    }
+  ]
+}
